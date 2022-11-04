da58bc2b3551aea1c525f570adc5bc1b4d04887a
bbland1 <104288486+bbland1@users.noreply.github.com>
2022-11-04 15:22:58 -0400

    updating contact form to work to send email

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from post import Post
 import requests
+import smtplib
 
 blog_url = "https://api.npoint.io/d6a237e8f73e6b93d7ed"
 blog_response = requests.get(blog_url)
 all_blog_posts = blog_response.json()
+
+email_contact_form = "YOUR_OWN_EMAIL"
+password_contact_form = "YOUR_OWN_PASSWORD"
 
 posts = []
 for post in all_blog_posts:
@@ -22,9 +26,31 @@ def home_page():
 def about_page():
     return render_template("about.html")
 
-@app.route("/contact")
+@app.route("/contact", methods=["GET", "POST"])
 def contact_page():
-    return render_template("contact.html")
+    if request.method == "POST":
+        data = request.form
+        send_email_from_from(data["name"], data["email"], data["message"])
+
+        print(data["name"])
+        print(data["email"])
+        print(data["message"])
+        return render_template("contact.html", message_sent=True)
+    return render_template("contact.html", message_sent=False)
+
+def send_email_from_from(name, email, email_message):
+    email_message = f"Subject: Blog Message \n\nName:{name}\nEmail:{email}\nMessage: {email_message}"
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(email_contact_form, password_contact_form)
+        connection.sendmail(
+            from_addr=email_contact_form,
+            to_addrs=email_contact_form,
+            message=email_message
+        )
+
+
+
 
 @app.route("/post/<int:num>")
 def post_page(num):
